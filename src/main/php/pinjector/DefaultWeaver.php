aff8f8b0c89b451fe7205e6cfeecb7c109da887d
Tobias Sarnowski <sarnowski@cosmocode.de>
2011-01-05 10:50:53 +0100

    copyright notice updated

--- src/main/php/pinjector/DefaultWeaver.php
+++ src/main/php/pinjector/DefaultWeaver.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright 2010 Tobias Sarnowski
+ * Copyright 2010,2011 Tobias Sarnowski
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- src/main/php/pinjector/Module.php
+++ src/main/php/pinjector/Module.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright 2010 Tobias Sarnowski
+ * Copyright 2010,2011 Tobias Sarnowski
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 require_once('Binder.php');
 
 /**
